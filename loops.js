994233c51a6e3cf9b5f4c18eba0cacdb8a493cdc
Luke Worley <luke.worley@colorado.edu>
2018-05-13 18:35:19 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,10 @@
 function forLoop(array){
-  
+  for (let i = 0; i < 25; i++) {
+  if (i==1){
+    array.unshift("I")
+  }
+}
+
 }
 function maybeTrue() {
   return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
