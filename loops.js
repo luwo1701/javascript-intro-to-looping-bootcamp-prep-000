952da70c0031e3b2d3a590cf8aa0c7080a10ed73
Luke Worley <luke.worley@colorado.edu>
2018-05-13 18:32:34 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ function maybeTrue() {
   return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
 }
 function doWhileLoop(arr){
-  while(arr.length>0 && maybeTrue){
+  while(arr.length>0 && maybeTrue(){
     arr.pop()
   }
 }
