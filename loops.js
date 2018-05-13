81e47cb3c9cc14a584b871bc35cabeb46bf2fc09
Luke Worley <luke.worley@colorado.edu>
2018-05-13 18:37:37 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,14 +1,19 @@
 function forLoop(array){
   for (let i = 0; i < 25; i++) {
-  if (i==1){
-    array.unshift("I am 1 strange loop.")
+    if (i==1){
+      array.unshift("I am 1 strange loop.")
+    }
+    else{
+      array.unshift("I am ${i} strange loops.")
+    }
   }
-  else{
-    array.unshift("I am ${i} strange loops.")
+}
+function whileLoop(n){
+  while(n>0){
+    
   }
 }
 
-}
 function maybeTrue() {
   return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
 }
